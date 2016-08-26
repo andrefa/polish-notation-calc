826a399109fbcb97768fa6ab818abcd7edca7f09
Andre Almeida <andre_de_almeida@epam.com>
2016-08-26 14:47:15 +0200

    logging functionality in ruby

--- logger.rb
+++ logger.rb
@@ -1,5 +1,29 @@
+require 'logger'
 require 'quartz'
 
-client = Quartz::Client.new(file_path: 'worker.go')
+$logger = Logger.new('logfile.log')
 
-puts client[:resolver].call('Calc', 'Expression'=>'1 2 +')['Result']
+def execute(expression)
+  startTime = Time.now
+  result = calculate(expression)
+  endTime = Time.now
+
+  duration = endTime - startTime
+
+  log(expression, result, duration)
+end
+
+def calculate(expression)
+  client = Quartz::Client.new(file_path: 'worker.go')
+  return client[:resolver].call('Calc', 'Expression'=>expression)['Result']
+end
+
+def log(expression, result, duration)
+  $logger.info "Expression '#{expression}' resulted '#{result}' in #{'%.04f' % duration}s."
+end
+
+def log_err(err)
+  $logger.error err
+end
+
+execute("2 3 *")
